7c2cd726f1164dd873376d8d1980bf4f9f7f6b44
linfeng <1029776125@qq.com>
2023-12-29 23:18:21 +0800

    inline method

--- src/main/java/com/tw/refactoring/composingmethods/InlineMethod.java
+++ src/main/java/com/tw/refactoring/composingmethods/InlineMethod.java
@@ -5,11 +5,6 @@ public class InlineMethod {
     private int numberOfLateDeliveries;
 
     int getRating() {
-        return moreThanFiveLateDeliveries() ? 2 : 1;
-    }
-
-    // the method body is more obvious than the method itself
-    boolean moreThanFiveLateDeliveries() {
-        return numberOfLateDeliveries > 5;
+        return numberOfLateDeliveries > 5 ? 2 : 1;
     }
 }
