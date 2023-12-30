e1bd9439c413e7380979bc680202f16d8454b44d
linfeng <1029776125@qq.com>
2023-12-30 09:34:17 +0800

    move field

---
+++ src/main/java/com/tw/refactoring/movingfeaturesbetweenobjects/MoveField.java
@@ -0,0 +1,8 @@
+package com.tw.refactoring.movingfeaturesbetweenobjects;
+
+public class MoveField {
+
+    // problem: a field is used more in another class than in its own class
+
+    // solution: move the field into the class that uses it the most
+}
