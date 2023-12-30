1c551aaeb26f4044ca7bb6a65d6a6221f59238ee
linfeng <1029776125@qq.com>
2023-12-30 09:22:17 +0800

    replace method with method object

--- src/main/java/com/tw/refactoring/composingmethods/ReplaceMethodWithMethodObject.java
+++ src/main/java/com/tw/refactoring/composingmethods/ReplaceMethodWithMethodObject.java
@@ -4,10 +4,26 @@ public class ReplaceMethodWithMethodObject {
 
     // a long method in which the local variables are so intertwined that you can't apply Extract Method
     public double price() {
-        double primaryBasePrice;
-        double secondaryBasePrice;
-        double tertiaryBasePrice;
-        // Perform long computation.
+        return new PriceCalculator(this).compute();
+    }
+}
+
+/**
+ * Transform the method into a separate class so that the local variables
+ * become fields of the class. Then you can split the method into several
+ * methods within the same class.
+ */
+class PriceCalculator {
+    double primaryBasePrice;
+    double secondaryBasePrice;
+    double tertiaryBasePrice;
+
+    public PriceCalculator(ReplaceMethodWithMethodObject object) {
+        // Copy relevant information from the order object.
+    }
+
+    public double compute() {
+        // Perform long computation
         return 0.0;
     }
 }
