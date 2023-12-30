8a420c5324f5323c63c166037824acad424dd0f3
linfeng <1029776125@qq.com>
2023-12-30 09:32:23 +0800

    move method

---
+++ src/main/java/com/tw/refactoring/movingfeaturesbetweenobjects/MoveMethod.java
@@ -0,0 +1,8 @@
+package com.tw.refactoring.movingfeaturesbetweenobjects;
+
+public class MoveMethod {
+
+    // problem: A method is used more in another class than in its own class
+
+    // solution: move the method into the class that uses it the most
+}
