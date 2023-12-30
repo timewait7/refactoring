d5e90dfe6bcfbe5a4f9fbd6bd2f4b86ab995c317
linfeng <1029776125@qq.com>
2023-12-30 10:17:15 +0800

    self encapsulate field

--- src/main/java/com/tw/refactoring/organizingdata/SelfEncapsulateField.java
+++ src/main/java/com/tw/refactoring/organizingdata/SelfEncapsulateField.java
@@ -8,6 +8,21 @@ class Range {
     private int high;
 
     boolean includes(int arg) {
-        return arg >= low && arg <= high;
+        return arg >= getLow() && arg <= getHigh();
+    }
+
+    /**
+     * indirect access to the fields is much more flexible
+     *
+     * first, lazy initialization and validation of fields can be implemented easily in the setters and getters
+     * second, you can redefine setters and getters in subclass
+     */
+
+    int getLow() {
+        return low;
+    }
+
+    int getHigh() {
+        return high;
     }
 }
