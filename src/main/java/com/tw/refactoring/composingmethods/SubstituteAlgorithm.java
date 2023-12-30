8ea8a3fff6995524ee6a24b2732cd88759641197
linfeng <1029776125@qq.com>
2023-12-30 09:27:45 +0800

    substitute algorithm

--- src/main/java/com/tw/refactoring/composingmethods/SubstituteAlgorithm.java
+++ src/main/java/com/tw/refactoring/composingmethods/SubstituteAlgorithm.java
@@ -1,17 +1,16 @@
 package com.tw.refactoring.composingmethods;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SubstituteAlgorithm {
 
     String foundPerson(String[] people){
+        List<String> candidates = Arrays.asList("Don", "John", "Kent");
         for (int i = 0; i < people.length; i++) {
-            if (people[i].equals("Don")){
-                return "Don";
-            }
-            if (people[i].equals("John")){
-                return "John";
-            }
-            if (people[i].equals("Kent")){
-                return "Kent";
+            if (candidates.contains(people[i])) {
+                return people[i];
             }
         }
         return "";
