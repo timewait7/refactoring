23c363faf66f005b41e11429bfb15c6c07309c69
linfeng <1029776125@qq.com>
2023-12-30 09:48:40 +0800

    hide delegate

--- src/main/java/com/tw/refactoring/movingfeaturesbetweenobjects/HideDelegate.java
+++ src/main/java/com/tw/refactoring/movingfeaturesbetweenobjects/HideDelegate.java
@@ -4,14 +4,17 @@ public class HideDelegate {
 
     String getDepartmentManager() {
         final Person1 person1 = new Person1();
-        final Department department = person1.getDepartment();
-        return department.getManager();
+        return person1.getManager();
     }
 }
 
 class Person1 {
     Department getDepartment() {
         return new Department();
+    }
+
+    String getManager() {
+        return getDepartment().getManager();
     }
 }
 
